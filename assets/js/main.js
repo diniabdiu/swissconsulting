eb2fd7cc5b81045dc772149c4f2246401726ca66
diniabdiu <diniabdiu@gmail.com>
2021-04-28 23:35:15 +0200

    fixed hamburger animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,97 +1,10 @@
+$(document).ready(function(){
+	$("#nav-icon4").click(function(){
+    $(this).toggleClass('open');
+	});
+});
 
 
- const slides=document.querySelector(".slideWrapp").children;
- const prev=document.querySelector(".prev");
- const next=document.querySelector(".next");
- const indicator=document.querySelector(".indicator");
- let index=0;
-
-
-   prev.addEventListener("click",function(){
-       prevSlide();
-       updateCircleIndicator(); 
-       resetTimer();
-   })
-
-   next.addEventListener("click",function(){
-      nextSlide(); 
-      updateCircleIndicator();
-      resetTimer();
-      
-   })
-
-   // create circle indicators
-    function circleIndicator(){
-        for(let i=0; i< slides.length; i++){
-        	const div=document.createElement("div");
-        	      div.innerHTML=i+1;
-                div.setAttribute("onclick","indicateSlide(this)")
-                div.id=i;
-                if(i==0){
-                	div.className="active";
-                }
-               indicator.appendChild(div);
-        }
-    }
-    circleIndicator();
-
-    function indicateSlide(element){
-         index=element.id;
-         changeSlide();
-         updateCircleIndicator();
-         resetTimer();
-    }
-     
-    function updateCircleIndicator(){
-    	for(let i=0; i<indicator.children.length; i++){
-    		indicator.children[i].classList.remove("active");
-    	}
-    	indicator.children[index].classList.add("active");
-    }
-
-   function prevSlide(){
-   	 if(index==0){
-   	 	index=slides.length-1;
-   	 }
-   	 else{
-   	 	index--;
-   	 }
-   	 changeSlide();
-   }
-
-   function nextSlide(){
-      if(index==slides.length-1){
-      	index=0;
-      }
-      else{
-      	index++;
-      }
-      changeSlide();
-   }
-
-   function changeSlide(){
-   	       for(let i=0; i<slides.length; i++){
-   	       	 slides[i].classList.remove("active");
-   	       }
-
-       slides[index].classList.add("active");
-   }
-
-   function resetTimer(){
-   	  // when click to indicator or controls button 
-   	  // stop timer 
-   	  clearInterval(timer);
-   	  // then started again timer
-   	  timer=setInterval(autoPlay,7000);
-   }
- 
-  
-  function autoPlay(){
-      nextSlide();
-      updateCircleIndicator();
-  }
-
-  let timer=setInterval(autoPlay,7000);
 
 //   navbar
 document.addEventListener("DOMContentLoaded", function(){
@@ -123,11 +36,6 @@ document.addEventListener("DOMContentLoaded", function(){
   }); 
   // DOMContentLoaded  end
 
-  $(document).ready(function(){
-	$('#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-	});
-});
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
@@ -139,12 +47,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-// const navLinks = document.querySelectorAll('.nav-item')
-// const menuToggle = document.getElementById('navbarSupportedContent')
-// const bsCollapse = new bootstrap.Collapse(menuToggle)
-// navLinks.forEach((l) => {
-//     l.addEventListener('click', () => { bsCollapse.toggle() })
-// })
+
 $(document).ready(function () {
   $(document).click(function (event) {
       var click = $(event.target);
